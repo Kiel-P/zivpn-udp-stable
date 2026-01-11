e27c9619802e6981520fbd7a87f57808af33dc6b
KIHEO <kielhjs6@gmail.com>
2026-01-11 18:28:58 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-
-systemctl stop udp-custom badvpn
-systemctl disable udp-custom badvpn
-
+systemctl stop zivpn-udp
+systemctl disable zivpn-udp
+rm -f /etc/systemd/system/zivpn-udp.service
 rm -f /usr/local/bin/udp-custom
 rm -f /usr/local/bin/badvpn-udpgw
-rm -rf /etc/zivpn
-rm -f /etc/systemd/system/udp-custom.service
-rm -f /etc/systemd/system/badvpn.service
-
-iptables -D INPUT -p udp --dport 5667 -j ACCEPT 2>/dev/null || true
-
+rm -rf /etc/zivpn /var/log/zivpn
 systemctl daemon-reload
-echo "[OK] UNINSTALLED"
+echo "Uninstalled"
